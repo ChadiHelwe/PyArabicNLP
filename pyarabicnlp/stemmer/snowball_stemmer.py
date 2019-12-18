bf0ef64a1663d6f0a5b7f2d82aca5188beb0305b
Chadi Helwe <chadi.helwe@gmail.com>
2019-12-18 04:55:48 +0200

    Added Snowball Stemmer but not implemented

---
+++ pyarabicnlp/stemmer/snowball_stemmer.py
@@ -0,0 +1,27 @@
+import snowballstemmer
+
+class SnowballStemmer:
+    """[summary]
+    """
+    def __init__(self):
+        self.stemmer = snowballstemmer.stemmer("arabic")
+
+    def stem_sentence(self, arabic_sentence: str) -> str:
+        """[summary]
+        
+        :param arabic_sentence: [description]
+        :type arabic_sentence: str
+        :return: [description]
+        :rtype: str
+        """
+        pass
+
+    def stem_word(self, arabic_word: str) -> str:
+        """[summary]
+        
+        :param arabic_word: [description]
+        :type arabic_word: str
+        :return: [description]
+        :rtype: str
+        """
+        pass
