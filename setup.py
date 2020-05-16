ddcc31792332a503ae65a212de0117d5d99b14cc
Chadi Helwe <chadi.helwe@gmail.com>
2020-05-16 23:02:50 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,11 @@
-from pip._internal.req import parse_requirements
-from pip._internal.download import PipSession
-import setuptools
+# from pip._internal.req import parse_requirements
+# from pip._internal.download import PipSession
+from distutils.extension import Extension
+from setuptools import setup, find_packages, Extension
 import os
+
+from Cython.Distutils import build_ext
+from Cython.Build import cythonize
 
 
 def read_requirements():
@@ -14,10 +18,13 @@ def read_requirements():
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+ext_modules = [
+    Extension("buckwalter", ["pyarabicnlp/transliteration/buckwalter.pyx"])
+]
 
-setuptools.setup(
+setup(
     name="pyarabicnlp",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="Chadi Helwe",
     author_email="chadi.helwe@gmail.com",
     description="PyArabicNLP: A Python library for Arabic NLP",
@@ -25,11 +32,15 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/ChadiHelwe/PyArabicNLP/",
     install_requires=read_requirements(),
-    packages=setuptools.find_packages(),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.6",
+    cmdclass={"build_ext": build_ext},
+    ext_modules=cythonize(
+        ext_modules, compiler_directives={"language_level": "3"}
+    ),
 )
