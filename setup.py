d016c1c1bfa7441072bcad5a44594eda994a3920
Chadi Helwe <chadi.helwe@gmail.com>
2020-05-16 23:18:49 +0300

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
-# from pip._internal.req import parse_requirements
-# from pip._internal.download import PipSession
+from pip._internal.req import parse_requirements
+from pip._internal.download import PipSession
 from distutils.extension import Extension
 from setuptools import setup, find_packages, Extension
 import os
